0ad2941f41324ae3bff9d8bbf494621970e117ff
Hồ Chí Nguyên <136091628+Nguyeniris123@users.noreply.github.com>
2025-07-17 09:31:46 +0700

    setup front end react native

---
+++ MMOApp_frontEnd/configs/Apis.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+Base_Url: "https://api.example.com";
+
+
+export const authApis = (token) => {
+    return axios.create({
+        baseURL: BASE_URL,
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+}
+
+export default axios.create({
+    baseURL: BASE_URL
+});
---
+++ MMOApp_frontEnd/styles/MyStyles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  }, subject: {
+    fontSize: 10,
+    fontWeight: "bold",
+    color: "#333",
+});
+
+export default styles;  
